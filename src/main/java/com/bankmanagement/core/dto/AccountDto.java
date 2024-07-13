e28d6d0c65ef41739022129083b7cb5eb321fbfd
artem <artem27082003@mail.ru>
2024-07-13 14:01:28 +0300

    validation for DTO

--- src/main/java/com/bankmanagement/core/dto/AccountDto.java
+++ src/main/java/com/bankmanagement/core/dto/AccountDto.java
@@ -4,6 +4,8 @@ import com.bankmanagement.core.enums.AccountStatus;
 import com.bankmanagement.entities.CardEntity;
 import com.bankmanagement.entities.TransactionEntity;
 import com.bankmanagement.entities.UserEntity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,12 +18,17 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountDto {
+    @NotNull
     private Integer id;
 
+    @NotNull(message = "Balance is required")
+    @PositiveOrZero(message = "Balance must be zero or positive")
     private Double balance;
 
+    @NotNull(message = "Account status is required")
     private AccountStatus status;
 
+    @NotNull(message = "User is required")
     private UserEntity user;
 
     private Set<CardEntity> cards;
--- src/main/java/com/bankmanagement/core/dto/UserDto.java
+++ src/main/java/com/bankmanagement/core/dto/UserDto.java
@@ -2,6 +2,10 @@ package com.bankmanagement.core.dto;
 
 import com.bankmanagement.entities.AccountEntity;
 import com.bankmanagement.entities.RoleEntity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,19 +18,27 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+    @NotNull
     private Integer id;
 
+    @NotBlank(message = "First name cannot be blank")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
 
+    @NotBlank(message = "Second name cannot be blank")
+    @Size(min = 2, max = 50, message = "Second name must be between 2 and 50 characters")
     private String secondName;
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
     private String email;
 
-    private String password;
-
+    @NotNull(message = "isBlocked cannot be null")
     private Boolean isBlocked;
 
+    @NotNull(message = "Roles cannot be null")
     private Set<RoleEntity> roles;
 
+    @NotNull(message = "Accounts cannot be null")
     private Set<AccountEntity> accounts;
 }
